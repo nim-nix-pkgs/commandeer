b073d9427ac414fee84deee83a85c408653d142a
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:50 -0300

    chore: re-index ../nimpkgs/c/commandeer/0_2_0

--- 0_2_0/flake.nix
+++ 0_2_0/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-commandeer-0_2_0.flake = false;
   inputs.src-commandeer-0_2_0.owner = "fenekku";
-  inputs.src-commandeer-0_2_0.ref   = "refs/tags/0.2.0";
+  inputs.src-commandeer-0_2_0.ref   = "0_2_0";
   inputs.src-commandeer-0_2_0.repo  = "commandeer";
   inputs.src-commandeer-0_2_0.type  = "github";
   
